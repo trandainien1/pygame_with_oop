90d6492cbeab53ec73b39c5d412f3cd52bcb64ef
trandainien1 <trandainien1@gmail.com>
2024-08-31 17:13:41 +0700

    add background

--- Flappy Bird/main.py
+++ Flappy Bird/main.py
@@ -1,14 +1,19 @@
 import sys
 import pygame
+from pygame.image import load
+from pygame.transform import scale_by
 
 pygame.init()
 
 # Global variables
 SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 800
+SCREEN_HEIGHT = 700
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Flappy bird')
 clock = pygame.time.Clock()
+background_surf = load('assets/background-night.png')
+background_surf = scale_by(background_surf, factor=2)
+background_rect = background_surf.get_rect()
 
 while True:
     for event in pygame.event.get():
@@ -16,6 +21,7 @@ while True:
             pygame.quit()
             sys.exit()
 
+    screen.blit(background_surf, background_rect)
 
     pygame.display.update()
     clock.tick(60)
