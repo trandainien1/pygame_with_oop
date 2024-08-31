999b9be6baab12b0605d46fb95a2d6cad9dee15e
trandainien1 <trandainien1@gmail.com>
2024-08-31 20:35:15 +0700

    score and reset game

--- Flappy Bird/main.py
+++ Flappy Bird/main.py
@@ -19,9 +19,14 @@ class Floor():
             self.rects.append(self.surf.get_rect(bottomleft=(padding, SCREEN_HEIGHT+20)))
         print(self.rects)
 
-    def update(self):
+    def update(self, bird_rect):
+        global playing
         for i in range(len(self.rects)):
             self.rects[i].x -= self.animation_speed
+
+            if bird_rect.colliderect(self.rects[i]):
+                playing = False
+
             if self.rects[i].right < 0:
                 self.rects[i].x = SCREEN_WIDTH
 
@@ -37,8 +42,8 @@ class Bird():
             self.surfs[i] = scale_by(self.surfs[i], factor=2)
         self.rect = self.surfs[self.surf_index].get_rect(center=(SCREEN_WIDTH//2, SCREEN_HEIGHT//2))
         self.velocity = 0
-        self.jump_height = -8
-        self.gravity = 0.3
+        self.jump_height = -6
+        self.gravity = 0.4
     def update(self, key_pressed):
         if key_pressed[pygame.K_SPACE]:
             self.velocity = self.jump_height
@@ -59,7 +64,7 @@ class Bird():
 class Pipe():
     def __init__(self):
         self.surf = scale_by(load('assets/pipe-green.png'), factor=2)
-        self.flipped_surf = pygame.transform.flip(scale_by(load('assets/pipe-green.png'), factor=2), flip_y=True, flip_x=False)
+        self.flipped_surf = scale_by(pygame.transform.flip(load('assets/pipe-green.png'), flip_y=True, flip_x=False), factor=2)
         self.rects = []
         self.flipped_rects = []
         self.heights = [450, 400, 350]
@@ -75,10 +80,14 @@ class Pipe():
         flipped_rect = self.flipped_surf.get_rect(bottomleft=(SCREEN_WIDTH, rect.top - 200))
         self.flipped_rects.append(flipped_rect)
 
-    def update(self):
+    def update(self, bird_rect):
+        global playing
         for i in range(len(self.rects)):
             self.rects[i].x -= self.animation_speed
             self.flipped_rects[i].x -= self.animation_speed
+
+            if bird_rect.colliderect(self.rects[i]) or bird_rect.colliderect(self.flipped_rects[i]):
+                playing = False
 
     def draw(self, screen):
         for i in range(len(self.rects)):
@@ -98,6 +107,13 @@ background_rect = background_surf.get_rect()
 floor = Floor()
 bird = Bird()
 pipe = Pipe()
+playing = True
+score = 0
+font = pygame.font.Font(None, 40)
+score_surf = font.render(f'Score: 0', False, 'White')
+score_rect = score_surf.get_rect(center=(SCREEN_WIDTH//2, 50))
+message_surf = scale_by(load('assets/message.png'), factor=2)
+message_rect = message_surf.get_rect(center=(SCREEN_WIDTH//2, SCREEN_HEIGHT//2))
 
 # Events
 SPAWN_PIPE_ENVENT = pygame.USEREVENT + 1
@@ -109,17 +125,34 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == SPAWN_PIPE_ENVENT:
+            score += 1
+            score_surf = font.render(f'Score: {score}', False, 'White')
             pipe.create_pipe()
+        if not playing:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    bird = Bird()
+                    pipe = Pipe()
+                    score = 0
+                    score_surf = font.render(f'Score: 0', False, 'White')
+                    playing = True
+
     key_pressed = pygame.key.get_pressed()
 
-    floor.update()
-    bird.update(key_pressed)
-    pipe.update()
+    if playing:
+        floor.update(bird.rect)
+        bird.update(key_pressed)
+        pipe.update(bird.rect)
 
     screen.blit(background_surf, background_rect)
-    pipe.draw(screen)
+    if playing:
+        pipe.draw(screen)
+        bird.draw(screen)
+        screen.blit(score_surf, score_rect)
+    else:
+        screen.blit(message_surf, message_rect)
+
     floor.draw(screen)
-    bird.draw(screen)
 
     pygame.display.update()
     clock.tick(60)
